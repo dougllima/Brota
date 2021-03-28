69e0227573a00d01c306058eaf7a95bd4ecaded6
Douglas Lima Leite <douglas_leite@sicredi.com.br>
2021-03-28 12:59:17 -0300

    Adicionando contexto de configurações.

--- src/contexts/configContext.tsx
+++ src/contexts/configContext.tsx
@@ -1,9 +1,26 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { minhasPlantinhas } from '../assets/data/Plantas';
+import { PlantaConfig } from '../model/Configs';
+import { NOMES_PLANTAS } from '../model/Planta';
 
 const { Consumer, Provider } = createContext({});
+const initialState = {} as Record<NOMES_PLANTAS, PlantaConfig>;
 
 export const ConfigProvider = (): React.ReactElement => {
-  return <Provider value={{}}></Provider>;
+  const [configPlantas, setConfigPlantas] = useState<
+    Record<NOMES_PLANTAS, PlantaConfig>
+  >(initialState);
+
+  useEffect(() => {
+    setConfigPlantas((config: Record<NOMES_PLANTAS, PlantaConfig>) => {
+      minhasPlantinhas.map(nome => {
+        config[nome] = new PlantaConfig();
+      });
+      return config;
+    });
+  }, []);
+
+  return <Provider value={{ configPlantas, setConfigPlantas }}></Provider>;
 };
 
 export { Consumer as ConfigConsumer };
--- src/model/Configs.ts
+++ src/model/Configs.ts
@@ -1,12 +1,22 @@
-export type PlantaConfig = {
-  cores: {
+export class PlantaConfig {
+  cores?: {
     colheita: string;
     germinacao: string;
     tempoDeVida: string;
   };
-  alertas: {
+  alertas?: {
     colheita: boolean;
     germinacao: boolean;
     tempoDeVida: boolean;
   };
-};
+
+  constructor(data: Partial<PlantaConfig> = {}) {
+    if (data) {
+      this.fromObject(data);
+    }
+  }
+
+  public fromObject(data = {}, instance = this): void {
+    Object.assign(instance, data);
+  }
+}
